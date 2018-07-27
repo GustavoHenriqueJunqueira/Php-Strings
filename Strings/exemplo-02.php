0a01d069f97508c95f366c7264ab21d55ac699dc
Gustavo Junqueria <gustavohenriquejunqueira@hotmail.com>
2018-07-27 10:09:30 -0300

    Funções antes da variavel
    $nome = "joão rangel";
    
    echo strtoupper($nome); // A função strtoupper converte os textos para letras maiusculas.
    
    echo "<br/>";
    
    $nome = strtolower ($nome); // A função strtolower converte os textos para letras minusculas.
    
    echo "<br/>";
    
    echo $nome;
    
    echo"<br/>";
    
    echo ucwords($nome); // Esta função converte apenas a primeira letra da parala para maiuscula.

---
+++ Strings/exemplo-02.php
@@ -0,0 +1,20 @@
+<?php
+
+$nome = "joão rangel";
+
+echo strtoupper($nome); // A função strtoupper converte os textos para letras maiusculas. 
+
+echo "<br/>";
+
+$nome = strtolower ($nome); // A função strtolower converte os textos para letras minusculas.
+
+echo "<br/>";
+
+echo $nome;
+
+echo"<br/>";
+
+echo ucwords($nome); // Esta função converte apenas a primeira letra da parala para maiuscula. 
+
+
+?>
