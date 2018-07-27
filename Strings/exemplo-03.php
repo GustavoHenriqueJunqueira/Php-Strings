51cd366bbf5d903aebdbda7b152dd1067695b9ce
Gustavo Junqueria <gustavohenriquejunqueira@hotmail.com>
2018-07-27 10:59:04 -0300

    Troca de caracter
    Neste exemplo 04 iremos ver como declarar uma variavel string e depois fazer alterações nela antes de exibir na tela.

---
+++ Strings/exemplo-03.php
@@ -0,0 +1,12 @@
+<?php
+
+$empresa = "Hcode";
+
+$empresa = str_replace("o", "0", $empresa); //Esta tag str_replace é utilizada para fazer mudanças e trocas de letras dentro de uma variavel já declarada.
+$empresa = str_replace("e", "3", $empresa);
+
+echo$empresa;
+
+echo"<br/>";
+
+?>
